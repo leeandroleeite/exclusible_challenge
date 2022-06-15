0e5b0e1d7ba39cb1e182313b4beded1c1fdf71f3
Leandro Leite <leeandro.leeite@gmail.com>
2022-06-15 15:53:26 +0100

    fix: updated db connection settings

--- index.ts
+++ index.ts
@@ -3,12 +3,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes/routes';
 
-// Connect to the database - MongoDB
-mongoose.connect('mongodb://localhost/exclusible', () => {
-    console.log('Connected to the database')
-})
+const dbURI = 'mongodb+srv://admin:IgK5I9OL05jsujUW@exclusible.j25bdwk.mongodb.net/exclusible?retryWrites=true&w=majority'
 
+// Connect to the database - MongoDB
+mongoose.connect(dbURI)
+.then(() => console.log('Connected to the database'))
+.catch(e => console.log(e));
+
+// Express app
 const app = express();
+
+// Config express to use json
+app.use(express.json())
+
+// Setup routes
 app.use('/api', routes)
 
+// Listen for requests
 app.listen(8000)
