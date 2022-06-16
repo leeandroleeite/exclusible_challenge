fc05815ab8ad7cd8de2f71aaabf51673e59b9bdd
Leandro Leite <leeandro.leeite@gmail.com>
2022-06-17 00:12:24 +0100

    feat: addedd new routes for retrieving a user list and removing all users from the database

--- routes/routes.ts
+++ routes/routes.ts
@@ -4,28 +4,34 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const router = express.Router()
-
 const JWT_KEY = 'secret'
 
-// Endpoint for user registration
+// User registration
 router.post('/register', async (req: Request, res: Response) => {
+    const user: any = await User.findOne({email: req.body.email})
+
+    if(user) {
+        return res.status(404).send({
+            message: 'user already exists'
+        })
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const user = new User ({
+    const newUser = new User ({
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword
     })
 
-    const result = await user.save()
+    const result = await newUser.save()
     const {password, ...data} = result.toJSON()
 
     res.send(data)
 })
 
-// Endpoint for user login
+// User login
 router.post('/login', async (req: Request, res: Response) => {
 
     const user: any = await User.findOne({email: req.body.email})
@@ -56,14 +62,12 @@ router.post('/login', async (req: Request, res: Response) => {
     })
 }) 
 
-// Authenticated user
+// User auth
 router.get('/user', async (req, res) => {
     try {
         const cookie = req.cookies['jwt']
-
         const claims: any = jwt.verify(cookie, JWT_KEY)
     
-        
         if(!claims) {
             return res.status(401).send({
                 message: 'unauthenticated'
@@ -74,17 +78,17 @@ router.get('/user', async (req, res) => {
         const {password, ...data} = await user.toJSON()
     
         res.send(data)
+
     } catch(e) {
         return res.status(401).send({
             message: 'unauthenticated'
         })
     }
-    
-
 })
 
 // User logout
 router.post('/logout', (req, res) => {
+
     res.cookie('jwt', '', {
         maxAge: 0,
     })
@@ -92,6 +96,25 @@ router.post('/logout', (req, res) => {
     res.send({
         message: 'logout successful'
     })
+})
+
+// Get a list of all registered users
+router.get('/users', async (req, res) => {
+    const users = await User.find({})
+
+    if(!users) {
+        return res.status(404).send({
+            message: 'user list is empty'
+        })
+    }
+    res.send(users)
+})
+
+// Remove all users
+router.delete('/drop', async (req, res) => {
+    const users = await User.remove()
+
+    res.send(users)
 })
 
 export default router
