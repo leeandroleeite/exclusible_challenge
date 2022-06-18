fe5410829abe9710e6e0777d1474f91fb78b12f9
Leandro Leite <leeandro.leeite@gmail.com>
2022-06-18 23:38:33 +0100

    feat: websocket for retrieving BTCUSD price from kraken

---
+++ api/web-socket/web-socket.ts
@@ -0,0 +1,40 @@
+import WebSocket from 'ws';
+
+let spread = 1.00;
+
+let publicWebSocketURL = "wss://ws.kraken.com/";
+let publicWebSocketSubscriptionMsg = '{ "event":"subscribe", "subscription":{"name":"spread"},"pair":["XBT/USD"] }';
+
+export async function OpenAndStreamWebSocketSubscription() {
+
+    try {
+        const webSocketClient = new WebSocket(publicWebSocketURL);
+
+        webSocketClient.on('open', function open() {
+            webSocketClient.send(publicWebSocketSubscriptionMsg);
+        });
+
+        webSocketClient.on('message', async function incoming(wsMsg) {
+            var date = new Date();
+            var msgTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+            var result = JSON.parse(wsMsg.toString());
+            var pair = result[3];
+            var values = result[1] || [];
+            var price = values[0] * spread;
+
+            if (price) {
+                console.log(msgTime, pair, price + ', spread: ' + spread)
+            }
+        });
+
+        webSocketClient.on('close', function close() {
+            console.log("WebSocket connection closed");
+        });
+
+    }
+    catch (e) {
+        console.log("AN EXCEPTION OCCURED :(");
+        console.log(e);
+    }
+};
